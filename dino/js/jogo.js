d760a856ee10548c22e3b6f6a04b0bb1bbb8b90e
Gabriel Radavelli <gabriel_radavelli@estudante.sc.senai.br>
2020-11-03 15:48:36 -0200

    Pulo do Dino

--- dino/js/jogo.js
+++ dino/js/jogo.js
@@ -1,1 +1,63 @@
 console.log('Inicio de Jogo!');
+
+const dino = document.querySelector('.dino');
+//console.log(dino);
+let dinoPosition = 0;
+let estaPulando = false;
+
+// Verificar se a tecla espaço foi pressionada
+document.addEventListener('keydown', (event)=>{
+    console.log(event.code)
+    if(event.code === 'Space')
+        if(!estaPulando) pular();
+});
+
+function pular(){
+    let intervaloPulo = setInterval(()=>{
+        estaPulando = true;
+        if(dinoPosition >= 250){
+            console.log('Topo!');
+            clearInterval(intervaloPulo);
+            let intervaloQueda = setInterval(()=>{
+                if (dinoPosition <= 0){
+                    console.log('Chão');
+                    estaPulando = false;
+                    clearInterval(intervaloQueda);
+                } else{
+                    //decrementar a posição do Dino
+                    dinoPosition -= 20;
+                    //Atualizar a posção na tela
+                    dino.style.bottom = dinoPosition + 'px';
+                }
+            },20);
+            
+        } else {
+            //incrementar a possição do Dino
+            dinoPosition += 20;
+            //Atualizar a possição da Tela
+            dino.style.bottom = dinoPosition + 'px';
+        }
+        
+    },20);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
